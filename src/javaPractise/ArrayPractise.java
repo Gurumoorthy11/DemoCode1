b794450ccb0828fa775c3835b50d752f5774b7f5
Gurumoorthy11 <62105033+Gurumoorthy11@users.noreply.github.com>
2021-06-19 12:57:50 +0530

    code pushed
    code pushed

---
+++ src/javaPractise/ArrayPractise.java
@@ -0,0 +1,14 @@
+package javaPractise;
+
+public class ArrayPractise {
+
+	public static void main(String[] args) {
+		 try {
+			int arr[]={1,2,3,4,5,6};
+			 System.out.println(arr.length);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/javaPractise/forLoop.java
+++ src/javaPractise/forLoop.java
@@ -5,12 +5,7 @@ public class forLoop {
 	public static void main(String args[]){
 		int arr[]=new int[]{1,2,3,4,5,6,7,8};
 		
-		System.out.print("[");
-		for(int i:arr){
-			
-			System.out.print(i+",");
-			
-		}
-		System.out.print("\t]");
+		System.out.print(arr.length);
+		
 	}
 }
